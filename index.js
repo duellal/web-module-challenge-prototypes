74929e6b508633b8718576206d27ec108e209d90
duellal <alexandriaduell@gmail.com>
2022-05-13 23:01:59 -0400

    Still Working On

--- index.js
+++ index.js
@@ -69,9 +69,17 @@ console.log(person3.stomach)
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
-
+function Car(model, milesPerGallon) {
+  this.model = model;
+  this.milesPerGallon = milesPerGallon;
+  this.tank = 0;
+  this.odometer = 0;
 }
+
+Car.prototype.fill = function(gallons){
+  this.tank = this.tank + gallons;
+}
+
 
 
 /*
